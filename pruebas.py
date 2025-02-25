9a6fada3c3c4276c43cf93587bddcbf49212b883
p-soutogz <pablosouto02@gmail.com>
2025-02-25 17:01:11 +0100

    primer commit

--- pruebas.py
+++ pruebas.py
@@ -1,16 +1,1 @@
-from transformers import pipeline
-from transformers import AutoTokenizer,AutoModelForSequenceClassification,BertTokenizer,BertModel
-
-classifier= pipeline("sentiment-analysis")
-
-res = classifier("I really love hugging face")
-
-model_name="distilbert-base-uncased-finetuned-sst-2-english"
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-classifier= pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-res = classifier("I really love hugging face")
-
-print(res)
+print("Hello World!!")
